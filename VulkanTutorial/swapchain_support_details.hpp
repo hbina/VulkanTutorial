e5d63cce5456ac5a4f5d185f8ec79f4f3cebc6eb
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2020-06-21 16:15:55 -0400

    Reworked some vertex member functions and added a FIXME note

--- VulkanTutorial/swapchain_support_details.hpp
+++ VulkanTutorial/swapchain_support_details.hpp
@@ -7,6 +7,7 @@
 // Struct for querying details of swap chain support.
 struct SwapChainSupportDetails
 {
+  // FIXME: Default values for this?
   VkSurfaceCapabilitiesKHR capabilities;
   std::vector<VkSurfaceFormatKHR> formats;
   std::vector<VkPresentModeKHR> presentModes;
--- VulkanTutorial/vertex.hpp
+++ VulkanTutorial/vertex.hpp
@@ -16,39 +16,39 @@ struct Vertex
   glm::vec3 color;
   glm::vec2 texCoord;
 
-  static VkVertexInputBindingDescription getBindingDescription()
+  static constexpr VkVertexInputBindingDescription getBindingDescription()
   {
-    VkVertexInputBindingDescription bindingDescription{};
-    bindingDescription.binding = 0;
-    bindingDescription.stride = sizeof(Vertex);
-    bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+    const VkVertexInputBindingDescription bindingDescription = {
+      .binding = 0,
+      .stride = sizeof(Vertex),
+      .inputRate = VK_VERTEX_INPUT_RATE_VERTEX
+    };
     return bindingDescription;
   }
 
-  static std::array<VkVertexInputAttributeDescription, 3>
+  static constexpr std::array<VkVertexInputAttributeDescription, 3>
   getAttributeDescriptions()
   {
-    std::array<VkVertexInputAttributeDescription, 3> attributeDescriptions{};
-
-    attributeDescriptions[0].binding = 0;
-    attributeDescriptions[0].location = 0;
-    attributeDescriptions[0].format = VK_FORMAT_R32G32B32_SFLOAT;
-    attributeDescriptions[0].offset = offsetof(Vertex, pos);
-
-    attributeDescriptions[1].binding = 0;
-    attributeDescriptions[1].location = 1;
-    attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
-    attributeDescriptions[1].offset = offsetof(Vertex, color);
-
-    attributeDescriptions[2].binding = 0;
-    attributeDescriptions[2].location = 2;
-    attributeDescriptions[2].format = VK_FORMAT_R32G32_SFLOAT;
-    attributeDescriptions[2].offset = offsetof(Vertex, texCoord);
-
+    const std::array<VkVertexInputAttributeDescription, 3>
+      attributeDescriptions = {
+        VkVertexInputAttributeDescription{ .location = 0,
+                                           .binding = 0,
+                                           .format = VK_FORMAT_R32G32B32_SFLOAT,
+                                           .offset = offsetof(Vertex, pos) },
+        VkVertexInputAttributeDescription{ .location = 1,
+                                           .binding = 0,
+                                           .format = VK_FORMAT_R32G32_SFLOAT,
+                                           .offset = offsetof(Vertex, color) },
+        VkVertexInputAttributeDescription{ .location = 2,
+                                           .binding = 0,
+                                           .format = VK_FORMAT_R32G32_SFLOAT,
+                                           .offset =
+                                             offsetof(Vertex, texCoord) }
+      };
     return attributeDescriptions;
   }
 
-  bool operator==(const Vertex& other) const
+  constexpr bool operator==(const Vertex& other) const
   {
     return pos == other.pos && color == other.color &&
            texCoord == other.texCoord;
